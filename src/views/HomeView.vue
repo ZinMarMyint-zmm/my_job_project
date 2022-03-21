2d719c150f2b69734ba7a02affe54eb69afa6b49
stellazmm <zmar9251@gmail.com>
2022-03-21 12:20:55 +0630

    404

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores
+      asperiores, reprehenderit molestiae perspiciatis enim omnis eius nesciunt
+      repellendus vitae doloremque fugit qui error illum nihil culpa. Omnis,
+      eius veritatis. Eveniet. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. A, maxime accusantium aliquid excepturi officiis ut eos
+      quasi nam deserunt, unde maiores, sit laboriosam commodi recusandae porro
+      nemo quidem nulla voluptatem adipisci architecto? Sit doloremque sequi
+      veritatis! Autem odio commodi error repellat, eligendi sapiente deleniti
+      temporibus nisi, dolores nemo, officia consequatur?
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores
+      asperiores, reprehenderit molestiae perspiciatis enim omnis eius nesciunt
+      repellendus vitae doloremque fugit qui error illum nihil culpa. Omnis,
+      eius veritatis. Eveniet. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. A, maxime accusantium aliquid excepturi officiis ut eos
+      quasi nam deserunt, unde maiores, sit laboriosam commodi recusandae porro
+      nemo quidem nulla voluptatem adipisci architecto? Sit doloremque sequi
+      veritatis! Autem odio commodi error repellat, eligendi sapiente deleniti
+      temporibus nisi, dolores nemo, officia consequatur?
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
---
+++ src/views/jobs/JobDetail.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Job Detail {{id}}</h1>
+</template>
+
+<script>
+export default {
+  props:['id']
+    // data(){
+    //   return{
+    //     id:this.$route.params.id
+    //   }
+    // }
+}
+</script>
+
+<style>
+
+</style>
